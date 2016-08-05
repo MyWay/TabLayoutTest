b7d6a5a3588986ef1189f22ac4f9c0de70ebb309
MyWay <gosustyle87@gmail.com>
2016-08-06 01:26:41 +0200

    Text added.

--- app/src/main/java/com/example/tablayout/tablayouttesting/MyFragment.java
+++ app/src/main/java/com/example/tablayout/tablayouttesting/MyFragment.java
@@ -2,9 +2,17 @@ package com.example.tablayout.tablayouttesting;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MyFragment extends Fragment {
     private static final String KEY_TITLE = "title";
+    private TextView mTextView;
 
     public static MyFragment newInstance(String sTitle) {
         MyFragment mFragment = new MyFragment();
@@ -15,6 +23,16 @@ public class MyFragment extends Fragment {
         mFragment.setArguments(args);
 
         return mFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mView = inflater.inflate(R.layout.fragment, container, false);
+        mTextView = (TextView) mView.findViewById(R.id.textView);
+        mTextView.setText(getTitle());
+
+        return mView;
     }
 
     public String getTitle() {
